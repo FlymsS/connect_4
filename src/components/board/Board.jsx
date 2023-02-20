3fe375faeb22481800158e50f6753614935a4875
DONOVAN <421126671@pcpuma.acatlan.unam.mx>
2023-02-19 23:31:06 -0600

    now push plays and check the winner

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,9 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./board.module.css";
 
-export default function Board() {
+export default function Board({plays, player, handleClickColumn}) {
+  const [columnMouseUp, setColumnMouseUp] = useState();
+  
   const x = 7;
-  const y = 6;  
+  const y = 6;
+
+  const onMouseMove = (e, id) => {
+    setColumnMouseUp(id);
+  };
+  const onMouseOut = () => {
+    setColumnMouseUp(null);
+  }
+
+  const getClass = (i,j) => {
+    if(plays[i][j] == 1) return styles.red;
+    if(plays[i][j] == 2) return styles.yellow;
+    if(plays[i][j] == 0 && columnMouseUp !== j) 
+      return styles.square;
+    if(plays[i][j] == 0 && columnMouseUp == j)
+      return styles.next;
+    //columnMouseUp == j? styles.next :
+  }
 
   const createBoard = () => {
     let board = [];
@@ -11,13 +30,16 @@ export default function Board() {
       let aux = [];
       for (let j = 0; j < x; j++) {
         aux.push(
-          <div className={styles.square} key={j * i}>
-            
-          </div>
+          <div
+            onMouseOver={(e) => onMouseMove(e, j)}
+            className={getClass(i,j)}
+            onClick={() => handleClickColumn(j)}
+            key={j + i}
+          ></div>
         );
       }
       board.push(
-        <div className={styles.row} key={i}>
+        <div id={i} className={styles.row} key={i}>
           {" "}
           {aux}{" "}
         </div>
@@ -26,7 +48,7 @@ export default function Board() {
     return board;
   };
   return (
-    <div className={styles.container}>
+    <div className={styles.container}  onMouseOut ={onMouseOut}>
       <div className={styles.board}>{createBoard()}</div>
     </div>
   );
--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,26 +1,79 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Board from "../board/Board";
 import styles from "./game.module.css";
 
 export default function Game() {
-  const [player, setPlayer] = useState(false);
 
-  const handlePlayer = () => {
-    setPlayer(!player);
-  };
-
-  const handleDrag = (e) => {
-    console.log(e)
+  const [player, setPlayer] = useState(1);
+  const [plays, setPlays] = useState(
+    [
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],])
+  const handleClickColumn = (column) => {
+    if(plays[0][column] != 0) return;
+    for(let i=5; i>=0; i--){
+      if(plays[i][column] == 0){
+        plays[i][column] = player;
+        setPlays([...plays]);
+        break;
+      }
+    }
+    checkWiner();
+    player == 1 ? setPlayer(2) : setPlayer(1);
   }
-
+  const checkWiner = () => {
+    // Check horizontal
+    for(let i=0; i<6; i++){
+      for(let j=0; j<4; j++){
+        if(plays[i][j] != 0 && plays[i][j] == plays[i][j+1] && plays[i][j] == plays[i][j+2] && plays[i][j] == plays[i][j+3] && plays[i][j+3] == player){
+          alert("player "+player+" win");
+          return;
+        }
+      }
+    }
+    // Check Vertical
+    for(let i=0; i<7; i++){
+      for(let j=0; j<3; j++){
+        if(plays[j][i] != 0 && plays[j][i] == plays[j+1][i] && plays[j][i] == plays[j+2][i] && plays[j][i] == plays[j+3][i] && plays[j+3][i] == player){
+          alert("player "+player+" win");
+          return;
+        }
+      }
+    }
+    // Check Diagonal1
+    for(let i=0; i<4; i++){
+      for(let j=0; j<3; j++){
+        if(plays[j][i] != 0 && plays[j][i] == plays[j+1][i+1] && plays[j][i] == plays[j+2][i+2] && plays[j][i] == plays[j+3][i+3] && plays[j+3][i+3] == player){
+          alert("player "+player+" win");
+          return;
+        }
+      }
+    }
+    // Check Diagonal2
+    for(let i=0; i<4; i++){
+      for(let j=3; j<6; j++){
+        if(plays[j][i] != 0 && plays[j][i] == plays[j-1][i+1] && plays[j][i] == plays[j-2][i+2] && plays[j][i] == plays[j-3][i+3] && plays[j-3][i+3] == player){
+          alert("player "+player+" win");
+          return;
+        }
+      }
+    }
+  }
   return (
     <div className={styles.container}>
-      <Board />
+      <Board 
+        player={player} 
+        plays={plays}
+        handleClickColumn={handleClickColumn}
+      />
       <div
         className={player ? styles.pieceRed : styles.pieceRed}
-        draggable="true"
-        onDragStart={handleDrag}
       ></div>
     </div>
   );
 }
+
